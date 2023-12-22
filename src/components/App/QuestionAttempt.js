513fbdfe849a850ba04dc468d2669608762f8a8a
kphamgit <kevinpham.us@gmail.com>
2023-12-21 21:45:32 -0500

    added React Player for videos (including youtube videos)

--- src/components/App/QuestionAttempt.js
+++ src/components/App/QuestionAttempt.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ButtonSelectQuestionAttempt from './ButtonSelectQA';
 import ClozeQuestionAttempt from './ClozeQA';
 import { Radio } from './Radio';
 import WordsScrambler from './WordsScrambler';
 import { useSelector } from 'react-redux'
+import ReactPlayer from 'react-player';
 
 function QuestionAttempt({format}) {
 
-  //console.log(" xxxxxxxxxxxxxxx"+format)
+ 
   const question = useSelector((state) => state.question.value)
-
+ 
   return (
     <div>
+  
       <div dangerouslySetInnerHTML={{ __html: question.instruction }}></div>
       <p style={{color:'brown'}}>{question.prompt}</p>
+      <div>
+      {question.audio_src && <audio src={question.audio_src} controls />}
+      </div> 
+      {question.video_src && <ReactPlayer url={question.video_src} controls />}
 
       {(() => {
         switch (format) {
